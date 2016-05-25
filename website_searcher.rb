15473bc44c4e46b8c42a0c648d6e3f2f6c0743b1
Oleg Kravtsov <o.api.qx@gmail.com>
2016-05-26 00:22:29 +0300

    Add loger

--- website_searcher.rb
+++ website_searcher.rb
@@ -1,21 +1,34 @@
 require 'csv'
 require 'thread'
+require 'uri'
+require 'net/http'
+require 'logger'
 
-THREAD_COUNT = 20
+THREAD_COUNT = 4
 
 FILE_INPUT = 'urls.txt'.freeze
 FILE_OUTPUT = 'results.txt'.freeze
 
+$logger = Logger.new('| tee logfile.log')
+if !ENV['DEBUG'].nil? && ENV['DEBUG'] == 'true'
+  $logger.level = Logger::DEBUG
+else
+  $logger.level = Logger::WARN
+end
+
 def main
   fail 'Please enter search term as a first param' if ARGV.empty?
-
+  $logger.info "Start"
   term = ARGV[0]
-  sites = Reader.read
+  sites = Data.read
+
+  $logger.info "term: #{term}"
 
   searcher = Searcher.new(sites, term)
-  result = searcher.perform
-  Data.save(result) if result.any?
-  p searcher.errors if searcher.errors.any?
+  results = searcher.perform
+  Data.write(results) if results.any?
+  $logger.warn "Errors with sites: #{searcher.errors}" if searcher.errors.any?
+  $logger.info "Finish"
 end
 
 class Data
@@ -23,13 +36,12 @@ class Data
     CSV.read(FILE_INPUT, headers: :first_row).map { |i| i['URL'] }
   end
 
-  def write(sites)
+  def self.write(sites)
     File.open(FILE_OUTPUT, 'w') do |f|
-      sites.each { |site| f.write(site) }
+      sites.each { |site| f.puts(site) }
     end
   rescue => e
-    p 'Unable to save file'
-    p e
+    $logger.error "Unable to save file: #{e}"
   end
 end
 
@@ -40,22 +52,22 @@ class Searcher
     @sites = sites
     @term = term
 
-    @result = []
+    @results = []
     @errors = []
 
     @queue = Queue.new
     @mutes ||= Mutex.new
+
+    @length = @sites.length
+    @count = 0
   end
 
   def perform
     @sites.each { |site| @queue << site }
     threads = []
     [THREAD_COUNT, @queue.length].min.times do
-      threads << Thread.new do
-        start_search
-      end
+      threads << Thread.new { start_search }
     end
-
     threads.map(&:join)
     @results
   end
@@ -63,12 +75,15 @@ class Searcher
   private
 
   def start_search
-    while site = @queue.pop(true)
-      Site.new(site)
-      if Site.check(@term)
-        @mutes.syncronize { @result << site }
+    while !@queue.empty? && url = @queue.pop
+      @mutes.synchronize { @count += 1 }
+      print "\e[34m#{@count}\e[0m\n"
+
+      site = Site.new(url)
+      if site.check(@term)
+        @mutes.synchronize { @results << site }
       else
-        @errors << Site.error if Site.has_error
+        @errors << site.error unless site.error.nil?
       end
     end
   end
@@ -76,31 +91,80 @@ end
 
 class Site
   attr_reader :url, :error
+  alias :to_s :url
+
   def initialize(url)
     @url = url
     @error = nil
+    parse_default_url
   end
 
   def check(term)
-    body = get_body
+    body = get_body(@uri)
     valid = false
     valid = check_body(body, term) unless body.nil?
+    $logger.debug "check: #{valid} - #{@url} "
     valid
   end
 
-  def get_body
-    uri = URI(@url)
-    response = Net::HTTP.get_response(uri)
-    case response
-    when Net::HTTPSuccess, Net::HTTPRedirection
-      response.body
+  private
+  def get_body(uri, limit = 10)
+    fail ArgumentError, 'HTTP redirect too deep' if limit == 0
+    $logger.debug uri.to_s
+    body = nil
+    begin
+      response = Net::HTTP.get_response(uri)
+      case response
+      when Net::HTTPSuccess
+        $logger.debug "Site #{@url}, body length: #{response.body.length}"
+        body = response.body
+      when Net::HTTPRedirection
+        $logger.debug "Redirect: #{11 - limit}"
+        uri = get_redirect_uri(response['location'])
+        body = get_body(uri, limit - 1)
+      end
+    rescue SocketError => e
+      $logger.warn("Site #{@url} SocketError: #{e}")
+    rescue Net::OpenTimeout => e
+      $logger.warn("Site #{@url} Net::OpenTimeout: #{e}")
+    rescue => e
+      $logger.error("Site #{@url} ERROR: #{e}")
+      @error = e
     end
-  rescue => e
-    @error = e
+    body
   end
 
-  def check_response(body, term)
-    Regexp.new(term, true) =~ body
+  def check_body(body, term)
+    !(Regexp.new(term, true) =~ body).nil?
+  end
+
+  def get_redirect_uri(url)
+    uri = URI(url)
+    # save redirected host/scheme
+    if uri.hostname.nil?
+      uri.hostname = @uri.hostname
+    else
+      @uri.hostname = uri.hostname
+    end
+
+    if uri.scheme.nil?
+      uri.scheme = @uri.scheme
+    else
+      @uri.scheme = uri.scheme
+    end
+
+    URI(uri.to_s)
+  end
+
+  def parse_default_url
+    @uri = URI(@url)
+    @uri.scheme = 'http' if @uri.scheme.nil?
+    if @uri.hostname.nil?
+      path = @uri.path.split('/')
+      @uri.hostname = path.pop
+      @uri.path = path.empty? ? '/' : path.join('/')
+    end
+    @uri = URI(@uri.to_s)
   end
 end
 
